3f22bac541deb08406cb0774938af7e7860765d3
Raven Blackhart <raven@cafiend.com>
2023-01-16 16:48:46 +0100

    New 2D Scene, Starting on UI; Ignore Teamsplit Script

---
+++ Assets/Scripts/TeamSplit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamSplit : MonoBehaviour
+{
+
+    private Alteruna.Avatar PlayerIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
