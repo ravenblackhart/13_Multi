2ef26f0ce3e2329f5394c0f8d381f5592a0831f8
Raven Blackhart <raven@cafiend.com>
2023-01-17 14:00:23 +0100

    stuff

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,31 +11,18 @@ public class UIManager : MonoBehaviour
     [SerializeField] private TMP_Text currentRoom; 
     [SerializeField] private TMP_Text playerScore;
     [SerializeField] private TMP_Text opponentScore;
+    [SerializeField] private TMP_Text roomTitle; 
     [SerializeField] private TMP_Text pauseButton;
     [SerializeField] private Button leaveButton;
-        
-        
-    [Header("RoomMenu")] 
-    [SerializeField] private Canvas roomMenu;
-    [SerializeField] private Button startButton;
-    [SerializeField] private Button joinButton;
-    [SerializeField] private TMP_Text titleText; 
-     
 
-    [Header("PauseMenu")] 
-     
+    [Header("RoomMenu")]
+    [SerializeField] private Canvas roomMenu; 
+
+    [Header("PauseMenu")]
     [SerializeField] private Canvas pauseMenu; 
     
     [Header("GameOverMenu")]
     [SerializeField] private Canvas gameOverMenu;
-    
-        
-
-    #region Alteruna
-    
-    
-
-    #endregion
 
 
 
@@ -49,6 +36,12 @@ public class UIManager : MonoBehaviour
         playerScore.text = "0";
         opponentScore.text = "0"; 
         
+    }
+
+    public void UpdateRoomName(string roomName)
+    {
+        roomTitle.text = roomName;
+        roomMenu.enabled = false; 
     }
 #region Base Functions
     public void Pause()
@@ -79,115 +72,7 @@ public class UIManager : MonoBehaviour
     {
         Application.Quit(); 
     }
-
 #endregion
 
-#region Alteruna Functions
-     private void Connected(Multiplayer multiplayer, Endpoint endpoint)
-    {
-        if (titleText != null)
-        {
-            titleText.text = "Rooms";
-        }
-
-        // if (isActiveAndEnabled)
-        // {
-        //     StartCoroutine(nameof(RefreshRooms));
-        // }
-    }
-
-    private void Disconnected(Multiplayer multiplayer, Endpoint endPoint)
-    {
-        if (titleText != null)
-        {
-            titleText.text = "Reconnecting..";
-        }
-    }
-
-    // private void UpdateList(Multiplayer multiplayer)
-    // {
-    //     for (int i = 0; i < _roomObjects.Count; i++)
-    //     {
-    //         Destroy(_roomObjects[i]);
-    //     }
-    //     _roomObjects.Clear();
-    //
-    //     if (ContentContainer != null)
-    //     {
-    //         for (int i = 0; i < multiplayer.AvailableRooms.Count; i++)
-    //         {
-    //             Room room = multiplayer.AvailableRooms[i];
-    //
-    //             GameObject entry;
-    //             if (room.Local)
-    //             {
-    //                 entry = Instantiate(WANEntryPrefab, ContentContainer.transform);
-    //             }
-    //             else
-    //             {
-    //                 entry = Instantiate(LANEntryPrefab, ContentContainer.transform);
-    //             }
-    //
-    //             entry.SetActive(true);
-    //             _roomObjects.Add(entry);
-    //
-    //             entry.GetComponentInChildren<Text>().text = room.Name;
-    //             entry.GetComponentInChildren<Button>().onClick.AddListener(() => { room.Join(); });
-    //         }
-    //     }
-    // }
-
-    private void JoinedRoom(Multiplayer multiplayer, Room room, User user)
-    {
-        if (titleText != null)
-        {
-            currentRoom.text = room.Name;
-        }
-    }
-
-    private void LeftRoom(Multiplayer multiplayer)
-    {
-        if (titleText != null)
-        {
-            titleText.text = "Rooms";
-        }
-    }
-
-    // private void Start()
-    // {
-    //     if (_aump == null)
-    //     {
-    //         _aump = FindObjectOfType<Multiplayer>();
-    //         if (!_aump)
-    //         {
-    //             Debug.LogError("Unable to find a active object of type Multiplayer.");
-    //         }
-    //     }
-    //
-    //     if (_aump != null)
-    //     {
-    //         _aump.Connected.AddListener(Connected);
-    //         _aump.Disconnected.AddListener(Disconnected);
-    //         _aump.RoomListUpdated.AddListener(UpdateList);
-    //         _aump.RoomJoined.AddListener(JoinedRoom);
-    //         _aump.RoomLeft.AddListener(LeftRoom);
-    //         startButton.onClick.AddListener(() => { _aump.JoinOnDemandRoom(); });
-    //         leaveButton.onClick.AddListener(() => { _aump.CurrentRoom?.Leave(); });
-    //     }
-    //
-    //     if (titleText != null)
-    //     {
-    //         titleText.text = "Connecting..";
-    //     }
-    // }
-    //
-    // private IEnumerator RefreshRooms()
-    // {
-    //     while (AutomaticallyRefresh) {
-    //         yield return new WaitForSeconds(RefreshInterval);
-    //         _aump.RefreshRoomList();
-    //     }
-    // }
-#endregion
 
 }
