c79cf628a35237cc05d0eb08bdffa813597ccf27
Stick <rutaspk@gmail.com>
2023-01-19 14:02:33 +0100

    ball attempted fix

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -7,11 +7,13 @@ public class BallController : MonoBehaviour
 {
     public float Speed = 0.01f;
     private bool moveBall = false;
-    private Vector3 direction = Vector3.left + Vector3.up + Vector3.left ;
+    private Vector2 direction = Vector2.left + Vector2.up + Vector2.left ;
 
     private Vector3 startPos;
-    
 
+    private Vector2 dir;
+    private Vector2 norml;
+    
     void Start()
     {
         startPos = transform.position;
@@ -24,11 +26,17 @@ public class BallController : MonoBehaviour
             Restart();
         }
 
-        if (Physics.Raycast(transform.position, direction, out RaycastHit hit, 0.5f))
+        Vector2 pos = transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(pos, direction, 1f);
+        if (hit.collider != null)
         {
-            PlayerController player = hit.transform.GetComponentInChildren<PlayerController>();
-            Vector3 incomingVec = hit.point - transform.position;
-            Vector3 reflectVec = Vector3.Reflect(incomingVec, hit.normal);
+            PlayerController2D player = hit.transform.GetComponent<PlayerController2D>();
+            Vector2 incomingVec = hit.point - pos;
+            Vector2 reflectVec = Vector2.Reflect(incomingVec, hit.normal);
+            
+            dir = direction;
+            norml = hit.normal;
             
             if (player)
             {
@@ -59,6 +67,17 @@ public class BallController : MonoBehaviour
         {
             moveBall = true;
         }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(dir.x, dir.y, 0) * 5);
+        
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(norml.x, norml.y, 0) * 5);
+
     }
 
     private void Restart()
